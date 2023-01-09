4d21feacd5d17d8af655a1786ef16e1311ef2e71
CatalinaML <listecatalina7@gmail.com>
2023-01-09 09:51:41 -0300

    Pool de conexion
    Agrego libreria apache commons y hago cambios para pool de conexiones

--- src/main/java/datos/Conexion.java
+++ src/main/java/datos/Conexion.java
@@ -2,14 +2,27 @@
 package datos;
 
 import java.sql.*;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 public class Conexion {
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/cursojava?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String JDBC_USER = "root";
     private static final String JDBC_PASS = "";
     
+    public static DataSource getDataSource(){
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(JDBC_URL);
+        dataSource.setUsername(JDBC_USER);
+        dataSource.setPassword(JDBC_PASS);
+        
+        //tamaño inicial del pool de conexiones
+        dataSource.setInitialSize(5); //no debe ser muy grande
+        return dataSource;
+    }
+    
     public static Connection getConnection() throws SQLException{
-        return DriverManager.getConnection(JDBC_URL,JDBC_USER,JDBC_PASS);
+        return getDataSource().getConnection();
     }
     
     public static void close(ResultSet rs) throws SQLException{
